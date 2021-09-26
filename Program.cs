8e51e0f116bebd4667984add7216445f4649bc7c
LuisSid55 <luiscitty4@gmail.com>
2021-09-26 19:21:06 -0300

    FGTS

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal salario, fgts;
+            
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n---FGTS---\n");
+            Console.ResetColor();
+            Console.WriteLine("Para calcular o FGTS, digite o salário abaixo\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Sálario: R$ ");
+            salario = Convert.ToDecimal(Console.ReadLine());
+
+            fgts = salario * 8 / 100; 
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"\nSeu FGTS: {fgts:C}\n");
+            Console.ResetColor();
+        }
+    }
+}
